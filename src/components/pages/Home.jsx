55364fb776a1e1166176875f1cf189f1063ea794
mohamdallam <mohamedahmedallam1@gmail.com>
2022-08-21 00:29:01 +0200

    category

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,61 +2,136 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../components/Styles/home.css";
+import "../../components/Styles/hero.css";
 import heroImg from "../../assets/images/hero.png";
+import Helmet from "./../Helmet/Helmet";
+import Category from "./../UI/Category/Category";
+
+import featureImg01 from "../../assets/images/service-01.png";
+import featureImg02 from "../../assets/images/service-02.png";
+import featureImg03 from "../../assets/images/service-03.png";
+
+const featureData = [
+  {
+    title: "Quick Delivery",
+    imgUrl: featureImg01,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+
+  {
+    title: "Super Dine In",
+    imgUrl: featureImg02,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+  {
+    title: "Easy Pick Up",
+    imgUrl: featureImg03,
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+  },
+];
+
 const Home = () => {
   return (
-    <section>
-      <Container>
-        <Row>
-          <Col lg="6" md="6">
-            <div className="hero__content  ">
-              <h5 className="mb-3">Easy way to make an order</h5>
-              <h1 className="mb-4 hero__title">
-                <span>HUNGRY?</span> Just wait <br /> food at
-                <span> your door</span>
-              </h1>
+    <Helmet>
+      <section>
+        <Container>
+          <Row>
+            {/* ======== Col 1 ========= */}
+            <Col lg="6" md="6">
+              <div className="hero__content  ">
+                <h5 className="mb-3">Easy way to make an order</h5>
+                <h1 className="mb-4 hero__title">
+                  <span>HUNGRY?</span> Just wait <br /> food at
+                  <span> your door</span>
+                </h1>
 
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                magni delectus tenetur autem, sint veritatis!
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+                  magni delectus tenetur autem, sint veritatis!
+                </p>
+
+                {/* Button */}
+                <div className="hero__btns d-flex align-items-center gap-5 mt-4">
+                  <button className="order__btn d-flex align-items-center justify-content-between">
+                    Order now <i class="ri-arrow-right-s-line"></i>
+                  </button>
+
+                  <button className="all__foods-btn">
+                    <Link to="/foods">See all foods</Link>
+                  </button>
+                </div>
+
+                {/* Text and icon under Button */}
+                <div className=" hero__service  d-flex align-items-center gap-5 mt-5 ">
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-car-line"></i>
+                    </span>
+                    No shipping charge
+                  </p>
+
+                  <p className=" d-flex align-items-center gap-2 ">
+                    <span className="shipping__icon">
+                      <i class="ri-shield-check-line"></i>
+                    </span>
+                    100% secure checkout
+                  </p>
+                </div>
+              </div>
+            </Col>
+
+            {/* ======== Col 2 ========= */}
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="hero-img" className="w-100" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* ======= Category ======= */}
+      <section>
+        <Category />-
+      </section>
+
+      {/* ======= Category ======= */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at home</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+              <p className="mb-1 mt-4 feature__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                officiis?
               </p>
+              <p className="feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, eius.{" "}
+              </p>
+            </Col>
 
-              <div className="hero__btns d-flex align-items-center gap-5 mt-4">
-                <button className="order__btn d-flex align-items-center justify-content-between">
-                  Order now <i class="ri-arrow-right-s-line"></i>
-                </button>
-
-                <button className="all__foods-btn">
-                  <Link to="/foods">See all foods</Link>
-                </button>
-              </div>
-
-              <div className=" hero__service  d-flex align-items-center gap-5 mt-5 ">
-                <p className=" d-flex align-items-center gap-2 ">
-                  <span className="shipping__icon">
-                    <i class="ri-car-line"></i>
-                  </span>
-                  No shipping charge
-                </p>
-
-                <p className=" d-flex align-items-center gap-2 ">
-                  <span className="shipping__icon">
-                    <i class="ri-shield-check-line"></i>
-                  </span>
-                  100% secure checkout
-                </p>
-              </div>
-            </div>
-          </Col>
-
-          <Col lg="6" md="6">
-            <div className="hero__img">
-              <img src={heroImg} alt="hero-img" className="w-100" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+            {featureData.map((item, index) => (
+              <Col lg="4" md="6" sm="6" key={index} className="mt-5">
+                <div className="feature__item text-center px-5 py-3">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className=" fw-bold mb-3">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   );
 };
 
